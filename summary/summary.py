4909ec66d1836dfed9732294df8466256ec6fe5b
Arne Binder <arne.b.binder@gmail.com>
2018-09-12 17:16:48 +0200

    outsourced conversion to html; replace (three) emojis with their html representations

--- summary/summary.py
+++ summary/summary.py
@@ -31,46 +31,61 @@ def answers_from_intents(intents):
                 yield a
 
 
-def prepare_for_html(s):
-    #s = html.escape(s)
-    s = re.sub(r'\[\s*([^\]]+)\s*\]', r'<a href="\1" target="_blank">\1</a>', s)
+def prepare_for_html(content, as_list=False):
+    if as_list:
+        s = '<ul>%s</ul>' % html.escape(content)
+        # add li elements
+        s = s.replace('##', '<li>').replace('||', '</li>')
+    else:
+        s = html.escape(content)
+    # replace links with captions
+    s = re.sub(r'\[LINK\]{{\s*([^}]+)\s*}}{{\s*([^}]+)\s*}} *', r'<a href="\1" target="_blank">\2</a> ',
+                           s)
+    # replace remaining links with captions
+    s = re.sub(r'\[LINK\]{{\s*([^}]+)\s*}} *', r'<a href="\1" target="_blank">\1</a> ', s)
+    # replace profile links (must have captions)
+    s = re.sub(r'\[LINK_PROFILE\]{{\s*([^}]+)\s*}}{{\s*([^}]+)\s*}}',
+                           r'<a class="profile-link" href="\1" target="_blank">\2</a>', s)
+    # replace images
+    s = re.sub(r'\[IMAGE\]{{\s*([^}]+)\s*}}', r'<img src="\1" />', s)
+
+    # replace quotes. That separates the list (add closing and opening ul tags)
+    if '[BLOCKQUOTE]' in s:
+        s = re.sub(r'schrieb:\s*', 'schrieb:<br/>', s)
+    if as_list:
+        s = re.sub(r'\[BLOCKQUOTE\]{{\s*([^}]+)\s*}}', r'</ul><blockquote>\1</blockquote><ul>', s)
+    else:
+        s = re.sub(r'\[BLOCKQUOTE\]{{\s*([^}]+)\s*}}', r'<blockquote>\1</blockquote>', s)
+
+    # replace emojis
+    # TODO: get list of all emojis
+    # see http://graphemica.com/%F0%9F%98%80
+    #s = s.replace('🙂', '&#128578;').replace('💕', '&#128578;').replace('😀', '&#128578;')
+    for emoji in ['🙂', '💕', '😀']:
+        s = s.replace(emoji, '&#%i;' % ord(emoji))
+    #s = s.replace('🙂', '').replace('💕', '').replace('😀', '')
     return s
 
 
-def intents_split_to_dynamicContent(intents_split, answers_all, num_posts, num_queries, query_nbr=None):
-    posts = [{'identifier': 'Post%i' % (i+1), 'type': 'TEXT', 'values': []} for i in range(num_posts)]
+def intents_split_to_dynamicContent(intents_split, answers_all, nbr_posts, max_queries, only_query_nbr=None):
+    posts = [{'identifier': 'Post%i' % (i+1), 'type': 'TEXT', 'values': []} for i in range(nbr_posts)]
     query = {'identifier': 'query', 'type': 'TEXT', 'values': []}
     for i, intent_id in enumerate(intents_split):
-        if i == num_queries:
+        if i == max_queries:
             break
-        if query_nbr is not None and i != query_nbr:
+        if only_query_nbr is not None and i != only_query_nbr:
             continue
         query['values'].append('<div class=\"query\">%s</div>' % prepare_for_html(intents_split[intent_id]['Intent-Text']))
         current_answers_split_sorted = sorted([(url, intents_split[intent_id]['answers_split'][url]) for url in
                                                intents_split[intent_id]['answers_split']], key=lambda x: len(''.join(x[1])))
-
-        for post_pos in range(num_posts):
+        for post_pos in range(nbr_posts):
             new_answer = ''
             #keys = list(intents_split[intent_id]['answers_split'].keys())
             if post_pos < len(current_answers_split_sorted):
                 answer_full = answers_all[current_answers_split_sorted[post_pos][0]]
-                answer_splits = '<ul>%s</ul>' % html.escape(current_answers_split_sorted[post_pos][1])
-                # add li elements
-                answer_splits = answer_splits.replace('##', '<li>').replace('||', '</li>')
-                # replace links with captions
-                answer_splits = re.sub(r'\[LINK\]{{\s*([^}]+)\s*}}{{\s*([^}]+)\s*}}', r'<a href="\1" target="_blank">\2</a>', answer_splits)
-                # replace remaining links with captions
-                answer_splits = re.sub(r'\[LINK\]{{\s*([^}]+)\s*}}', r'<a href="\1" target="_blank">\1</a>', answer_splits)
-                # replace profile links (must have captions)
-                answer_splits = re.sub(r'\[LINK_PROFILE\]{{\s*([^}]+)\s*}}{{\s*([^}]+)\s*}}', r'<a class="profile-link" href="\1" target="_blank">\2</a>', answer_splits)
-                # replace images
-                answer_splits = re.sub(r'\[IMAGE\]{{\s*([^}]+)\s*}}', r'<img src="\1" />', answer_splits)
-
-                # replace quotes. That separates the list (add closing and opening ul tags)
-                answer_splits = re.sub(r'\[BLOCKQUOTE\]{{\s*([^}]+)\s*}}', r'</ul><blockquote>\1</blockquote><ul>', answer_splits)
-
-                #replace emojies
-                answer_splits = answer_splits.replace('🙂', '').replace('💕', '').replace('😀', '')
+                #answer_splits = prepare_for_html(current_answers_split_sorted[post_pos][1], as_list=True)
+                # remove post counts (like "(4)") at the end and convert to html
+                answer_splits = prepare_for_html(re.sub(r'\s*\(\d+\)\s*$', '', current_answers_split_sorted[post_pos][1]), as_list=True)
 
                 new_answer = '<div class="answer-content">%s</div>' % answer_splits
                 question_title = '<div class="question-title">%s</div>' % answer_full['question']['title']
@@ -107,7 +122,7 @@ if __name__ == "__main__":
     intents_split = {intent['Intent-ID']: {'Intent-Text': intent['Intent-Text'], 'answers_split': answer_from_concat(intent[column_split_content])} for intent in read_tsv(path.join(path_base, tsv_sentences_fn))}
     with open(path.join('summary', 'Summary.template.json')) as f:
         summary = json.load(f)
-    summary['dynamicContent'] = intents_split_to_dynamicContent(intents_split, answers_all, num_posts=10, num_queries=10)#, query_nbr=6)
+    summary['dynamicContent'] = intents_split_to_dynamicContent(intents_split, answers_all, nbr_posts=10, max_queries=10)#, only_query_nbr=6)
     #with open('scrape_10/Summary_content.json', 'w') as f:
     with codecs.open(path.join(path_base, 'Summary.json'), 'w', encoding='utf-8') as f:
         json.dump(summary, f, ensure_ascii=False, indent=4)
