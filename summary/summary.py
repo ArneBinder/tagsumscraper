6f211a1300e73e7dba0f2269ce27f45874d58729
Arne Binder <arne.b.binder@gmail.com>
2018-09-21 12:23:51 +0200

    added summary_good/summary_bad dummies

--- summary/summary.py
+++ summary/summary.py
@@ -8,6 +8,10 @@ from questionscraper.spiders.helper import load_jl
 from os import path
 
 
+FORMAT_LIST = 'list'
+FORMAT_CHECKBOXES = 'checkboxes'
+
+
 def read_tsv(path):
     with open(path) as tsvfile:
         reader = csv.DictReader(tsvfile, delimiter='\t')
@@ -32,11 +36,13 @@ def answers_from_intents(intents):
                 yield a
 
 
-def prepare_for_html(content, as_list=False):
-    if as_list:
+def prepare_for_html(content, format_as=FORMAT_LIST):
+    if format_as == FORMAT_LIST:
         s = '<ul>%s</ul>' % html.escape(content)
         # add li elements
         s = s.replace('##', '<li>').replace('||', '</li>')
+    elif format_as == FORMAT_CHECKBOXES:
+        s = content.replace('##', '<p><input type="checkbox" name="check">').replace('||', '</p>')
     else:
         s = html.escape(content)
     # replace links with captions
@@ -53,7 +59,8 @@ def prepare_for_html(content, as_list=False):
     # replace quotes. That separates the list (add closing and opening ul tags)
     if '[BLOCKQUOTE]' in s:
         s = re.sub(r'schrieb:\s*', 'schrieb:<br/>', s)
-    if as_list:
+    # interrupt the list for blockquotes, if lists are is used
+    if format_as == FORMAT_LIST:
         s = re.sub(r'\[BLOCKQUOTE\]{{\s*([^}]+)\s*}}', r'</ul><blockquote>\1</blockquote><ul>', s)
     else:
         s = re.sub(r'\[BLOCKQUOTE\]{{\s*([^}]+)\s*}}', r'<blockquote>\1</blockquote>', s)
@@ -68,15 +75,22 @@ def prepare_for_html(content, as_list=False):
     return s
 
 
-def intents_split_to_dynamicContent(intents_split, answers_all, nbr_posts, max_queries, only_query_nbr=None):
+def intents_split_to_dynamicContent(intents_split, answers_all, nbr_posts, max_queries, only_query_nbr=None, format_as=FORMAT_LIST):
     posts = [{'identifier': 'Post%i' % (i+1), 'type': 'TEXT', 'values': []} for i in range(nbr_posts)]
     query = {'identifier': 'query', 'type': 'TEXT', 'values': []}
+    summary_good = {'identifier': 'summaryGood', 'type': 'TEXT', 'values': []}
+    summary_bad = {'identifier': 'summaryBad', 'type': 'TEXT', 'values': []}
     for i, intent_id in enumerate(intents_split):
         if i == max_queries:
             break
         if only_query_nbr is not None and i != only_query_nbr:
             continue
         query['values'].append('<div class=\"query\">%s</div>' % prepare_for_html(intents_split[intent_id]['Intent-Text']))
+        # TODO: change this!
+        current_summary_good = 'good DUMMY SUMMARY for intent %s' % intent_id
+        current_summary_bad = 'bad DUMMY SUMMARY for intent %s' % intent_id
+        summary_good['values'].append('<div class=\"summary\">%s</div>' % current_summary_good)
+        summary_bad['values'].append('<div class=\"summary\">%s</div>' % current_summary_bad)
         current_answers_split_sorted = sorted([(url, intents_split[intent_id]['answers_split'][url]) for url in
                                                intents_split[intent_id]['answers_split']], key=lambda x: len(''.join(x[1])))
         for post_pos in range(nbr_posts):
@@ -86,7 +100,7 @@ def intents_split_to_dynamicContent(intents_split, answers_all, nbr_posts, max_q
                 answer_full = answers_all[current_answers_split_sorted[post_pos][0]]
                 #answer_splits = prepare_for_html(current_answers_split_sorted[post_pos][1], as_list=True)
                 # remove post counts (like "(4)") at the end and convert to html
-                answer_splits = prepare_for_html(re.sub(r'\s*\(\d+\)\s*$', '', current_answers_split_sorted[post_pos][1]), as_list=True)
+                answer_splits = prepare_for_html(re.sub(r'\s*\(\d+\)\s*$', '', current_answers_split_sorted[post_pos][1]), format_as=format_as)
 
                 new_answer = '<div class="answer-content">%s</div>' % answer_splits
                 question_title = '<div class="question-title">%s</div>' % answer_full['question']['title']
@@ -101,14 +115,15 @@ def intents_split_to_dynamicContent(intents_split, answers_all, nbr_posts, max_q
                 new_answer = question_title + new_answer
             posts[post_pos]['values'].append(new_answer)
 
-    return posts + [query]
+    return posts + [query, summary_good, summary_bad]
 
 
 def main(base_path: ("Path to the base directory", 'option', 'p')='scrape_10',
          tsv_sentences_fn: ("tsv file containing the split sentences", 'option', 's')='ARNE_LEO-Training-Example-Summaries-Intents.tsv',
          intents_all_fn: ("Jsonline file containing all intent data", 'option', 'i')='intents_questions_10_merged.jl',
          summary_template_fn: ("Json file that will be used as template", 'option', 't')='Summary.template.json',
-         column_split_content: ("Column in the tsv sentences file that contains the split sentences", 'option', 'c')='answers_plain_marked_relevant_NEW'
+         column_split_content: ("Column in the tsv sentences file that contains the split sentences", 'option', 'c')='answers_plain_marked_relevant_NEW',
+         format_as: ("How to format the sentence entries", 'option', 'f', str, [FORMAT_LIST, FORMAT_CHECKBOXES])=FORMAT_LIST
          ):
 
     template_marker = '.template'
@@ -127,7 +142,14 @@ def main(base_path: ("Path to the base directory", 'option', 'p')='scrape_10',
     intents_split = {intent['Intent-ID']: {'Intent-Text': intent['Intent-Text'], 'answers_split': answer_from_concat(intent[column_split_content])} for intent in read_tsv(path.join(base_path, tsv_sentences_fn))}
     with open(path.join(base_path, summary_template_fn)) as f:
         summary = json.load(f)
-    summary['dynamicContent'] = intents_split_to_dynamicContent(intents_split, answers_all, nbr_posts=10, max_queries=10)#, only_query_nbr=6)
+
+    #with codecs.open(path.join(base_path, 'debug.json'), 'w', encoding='utf-8') as f:
+    #    json.dump(summary, f, ensure_ascii=False, indent=4)
+    #    #f.write(json_string)
+    #    f.flush()
+
+    summary['dynamicContent'] = intents_split_to_dynamicContent(intents_split, answers_all, nbr_posts=10,
+                                                                max_queries=10, format_as=format_as)#, only_query_nbr=6)
     #with open('scrape_10/Summary_content.json', 'w') as f:
     with codecs.open(path.join(base_path, summary_out_fn), 'w', encoding='utf-8') as f:
         json.dump(summary, f, ensure_ascii=False, indent=4)
