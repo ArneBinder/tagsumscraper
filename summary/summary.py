9e96c8fe13bd32d024c8e0d3d0b8b8586bcec7b3
Arne Binder <arne.b.binder@gmail.com>
2018-09-21 13:01:24 +0200

    added FORMAT_PARAGRAPHS and removed FORMAT_CHECKBOXES

--- summary/summary.py
+++ summary/summary.py
@@ -9,7 +9,8 @@ from os import path
 
 
 FORMAT_LIST = 'list'
-FORMAT_CHECKBOXES = 'checkboxes'
+#FORMAT_CHECKBOXES = 'checkboxes'
+FORMAT_PARAGRAPHS = 'paragraphs'
 
 
 def read_tsv(path):
@@ -40,9 +41,10 @@ def prepare_for_html(content, format_as=FORMAT_LIST):
     if format_as == FORMAT_LIST:
         s = '<ul>%s</ul>' % html.escape(content)
         # add li elements
-        s = s.replace('##', '<li>').replace('||', '</li>')
-    elif format_as == FORMAT_CHECKBOXES:
-        s = content.replace('##', '<p><input type="checkbox" name="check">').replace('||', '</p>')
+        s = s.replace('##', '<li><span class="sentence">').replace('||', '</span></li>')
+    elif format_as == FORMAT_PARAGRAPHS:
+        #s = content.replace('##', '<p><input type="checkbox" name="%s">' % meta['checkbox_name']).replace('||', '</p>')
+        s = content.replace('##', '<p><span class="sentence">').replace('||', '</span></p>')
     else:
         s = html.escape(content)
     # replace links with captions
@@ -89,6 +91,7 @@ def intents_split_to_dynamicContent(intents_split, answers_all, nbr_posts, max_q
         # TODO: change this!
         current_summary_good = 'good DUMMY SUMMARY for intent %s' % intent_id
         current_summary_bad = 'bad DUMMY SUMMARY for intent %s' % intent_id
+
         summary_good['values'].append('<div class=\"summary\">%s</div>' % current_summary_good)
         summary_bad['values'].append('<div class=\"summary\">%s</div>' % current_summary_bad)
         current_answers_split_sorted = sorted([(url, intents_split[intent_id]['answers_split'][url]) for url in
@@ -100,7 +103,8 @@ def intents_split_to_dynamicContent(intents_split, answers_all, nbr_posts, max_q
                 answer_full = answers_all[current_answers_split_sorted[post_pos][0]]
                 #answer_splits = prepare_for_html(current_answers_split_sorted[post_pos][1], as_list=True)
                 # remove post counts (like "(4)") at the end and convert to html
-                answer_splits = prepare_for_html(re.sub(r'\s*\(\d+\)\s*$', '', current_answers_split_sorted[post_pos][1]), format_as=format_as)
+                answer_splits = prepare_for_html(re.sub(r'\s*\(\d+\)\s*$', '', current_answers_split_sorted[post_pos][1]),
+                                                 format_as=format_as)
 
                 new_answer = '<div class="answer-content">%s</div>' % answer_splits
                 question_title = '<div class="question-title">%s</div>' % answer_full['question']['title']
@@ -123,7 +127,7 @@ def main(base_path: ("Path to the base directory", 'option', 'p')='scrape_10',
          intents_all_fn: ("Jsonline file containing all intent data", 'option', 'i')='intents_questions_10_merged.jl',
          summary_template_fn: ("Json file that will be used as template", 'option', 't')='Summary.template.json',
          column_split_content: ("Column in the tsv sentences file that contains the split sentences", 'option', 'c')='answers_plain_marked_relevant_NEW',
-         format_as: ("How to format the sentence entries", 'option', 'f', str, [FORMAT_LIST, FORMAT_CHECKBOXES])=FORMAT_LIST
+         format_as: ("How to format the sentence entries", 'option', 'f', str, [FORMAT_LIST, FORMAT_PARAGRAPHS])=FORMAT_LIST
          ):
 
     template_marker = '.template'
